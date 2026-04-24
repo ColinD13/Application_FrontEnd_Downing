88594d1fd728a7a6669d9626a6470cab4a713bae
ColinD13 <downingcolin13@gmail.com>
2026-04-24 19:09:04 -0400

    Added delete for each table
    Delete for player, user, and trade (trade_player included in trade)

---
+++ js/deletePlayer.js
@@ -0,0 +1,34 @@
+const formEl = document.querySelector(".form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(formEl);
+  const data = Object.fromEntries(formData);
+
+  if (data.player_id == "") {
+    $.toaster({
+      priority: "danger",
+      title: "Error Message",
+      message: "All values need inputted",
+    });
+  } else {
+    console.log("Submitting:", data);
+    fetch("https://application-backend-downing.onrender.com/api/players", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        player_id: data.player_id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .then((error) => console.log());
+    $.toaster({
+      priority: "success",
+      title: "Success Message",
+      message: "Values have been submitted",
+    });
+  }
+});
---
+++ js/deleteTrade.js
@@ -0,0 +1,34 @@
+const formEl = document.querySelector(".form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(formEl);
+  const data = Object.fromEntries(formData);
+
+  if (data.user_id == "") {
+    $.toaster({
+      priority: "danger",
+      title: "Error Message",
+      message: "All values need inputted",
+    });
+  } else {
+    console.log("Submitting:", data);
+    fetch("https://application-backend-downing.onrender.com/api/trades", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        trade_id: data.trade_id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .then((error) => console.log());
+    $.toaster({
+      priority: "success",
+      title: "Success Message",
+      message: "Values have been submitted",
+    });
+  }
+});
